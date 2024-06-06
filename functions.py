46a3ca487bb1b3c049a5092f8a20e1ccc9b5335a
sweetpinejam <sweetpinejam@gmail.com>
2024-06-06 22:27:17 +0700

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,159 @@
+import streamlit as st
+from googleapiclient.discovery import build
+import google.generativeai as genai
+import sqlite3
+from yake import KeywordExtractor
+import cohere
+
+genai.configure(api_key=st.secrets["genai_api_key"])
+
+def generate_tags():
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    tags_prompt = 'Generate random educational tags like this subject1, subject2, subject3, subject4 etc. just plain text. just a few of them. bring advanced and diverse subject'
+    response = model.generate_content(tags_prompt)
+    response = response.text.split(', ')
+    return response
+
+def create_query_gemini(keywords):
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    tags_prompt = f'Generate a topic to search with these keywords {keywords}. Generate in simple plain text. No colon just texts. Make it creative.'
+    response = model.generate_content(tags_prompt)
+    return response.text
+
+def create_query_cohere(keywords):
+    api_key = st.secrets["cohere_api_key"]
+    co = cohere.Client(api_key)
+    prompt = f"Please create a question for YouTube in just a single sentence in plain text from these topics {keywords}. For education"
+    response = co.generate(
+        model="command-light",
+        prompt=prompt,
+        max_tokens=40,
+        temperature=0.8
+    )
+    return response.generations[0].text
+
+def create_query_parallel(keywords):
+    query = ''
+    try:
+        query = create_query_gemini(keywords)
+    except Exception:
+        try:
+            query = create_query_cohere(keywords)
+        except Exception:
+            st.error("Exceed LLMs APIs Limit. Please Wait for a minute.")
+    finally:
+        return query
+
+def search_videos(query, api_key=st.secrets["youtube_api_key"]):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    search_response = youtube.search().list(
+        q=query,
+        part='snippet',
+        type='video',
+        maxResults=6
+    ).execute()
+    return search_response.get('items', [])
+
+def extract_keywords(query):
+    extractor = KeywordExtractor()
+    keywords = extractor.extract_keywords(query)
+    return [key[0] for key in keywords]
+
+def setup_database():
+    conn = sqlite3.connect('.venv/queries.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS queries (
+            id INTEGER PRIMARY KEY,
+            query TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def save_query(query):
+    conn = sqlite3.connect('.venv/queries.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO queries (query) VALUES (?)', (query,))
+    conn.commit()
+    conn.close()
+
+def get_last_query():
+    conn = sqlite3.connect('.venv/queries.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT query FROM queries ORDER BY id DESC LIMIT 1')
+    result = cursor.fetchone()
+    conn.close()
+    if result:
+        return result[0]
+    return ""
+
+def deep_search():
+    setup_database()
+
+    st.markdown(
+        """
+        <style>
+            .st-d6 {
+                display: flex;
+                align-items: center;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                padding: 8px;
+                margin: 10px 0;
+            }
+            .st-d7 {
+                flex: 1;
+                border: none;
+                outline: none;
+                padding: 0 8px;
+            }
+            .st-d8 {
+                background-color: #ff0000;
+                color: #fff;
+                border: none;
+                border-radius: 5px;
+                padding: 8px;
+                cursor: pointer;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    # Get the search query from the user
+    search_query = st.text_input("Search", "", key="search_input")
+
+    # Handle the search button click
+    if st.button("Search", key="search_button"):
+        save_query(search_query)
+
+    # Read the last query from the database
+    query = get_last_query()
+
+    # Define a function to process and display videos
+    def display_videos(query):
+        keywords = extract_keywords(query)
+        refined_query = create_query_parallel(keywords)
+        print(refined_query)
+        videos = search_videos(refined_query)
+        for video in videos:
+            video_title = video['snippet']['title']
+            video_id = video['id']['videoId']
+            video_url = f"https://www.youtube.com/watch?v={video_id}"
+            st.subheader(video_title)
+            st.video(video_url)
+        return refined_query
+
+    # Display videos for the initial search query
+    display_videos(query)
+
+    # Handle Filter 1 button click
+    if st.button('Apply Filter 1'):
+        refined_query1 = display_videos(query)
+        save_query(refined_query1)
+
+    # Handle Filter 2 button click
+    if st.button('Apply Filter 2'):
+        refined_query2 = display_videos(query)
+        save_query(refined_query2)
---
+++ main.py
@@ -0,0 +1,179 @@
+import streamlit as st
+from googleapiclient.discovery import build
+from requests.exceptions import RequestException
+from functions import *
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Set the dark theme and page config
+st.set_page_config(page_title="EduTube Demo", layout="centered", initial_sidebar_state="collapsed")
+
+# Apply custom CSS for dark theme and brighter headers
+st.markdown("""
+    <style>
+        body {
+            background-color: #DCDCDC;
+            color: #ffffff;
+            font-family: 'Arial', sans-serif;
+        }
+        .stApp {
+            background-color: #DCDCDC;
+        }
+        .css-18e3th9 {
+            background-color: #DCDCDC;
+        }
+        .css-1d391kg {
+            background-color: #DCDCDC;
+        }
+        .stButton>button {
+            background-color: #1f1f1f;
+            color: #ffffff;
+            border: 1px solid #ffffff;
+            border-radius: 10px;
+            padding: 10px 20px;
+            font-size: 16px;
+            margin-top: 20px;
+        }
+        .stButton>button:hover {
+            background-color: #333333;
+        }
+        .stSelectbox>div {
+            background-color: #1f1f1f;
+            color: #ffffff;
+            border: 1px solid #ffffff;
+            border-radius: 10px;
+            padding: 10px;
+            font-size: 16px;
+        }
+        .stTextInput>div>div input {
+            background-color: #1f1f1f;
+            color: #ffffff;
+            border: 1px solid #ffffff;
+            border-radius: 10px;
+            padding: 10px;
+            font-size: 16px;
+        }
+        .stMarkdown h1, .stMarkdown h2, .stMarkdown h3 {
+            color: #FFFAF0;
+        }
+        .stVideo {
+            border-radius: 10px;
+        }
+        .sidebar .sidebar-content {
+            background-color: #1f1f1f;
+            color: #ffffff;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Structure of the Website
+if 'is_logged_in' not in st.session_state:
+    st.session_state.is_logged_in = False
+
+if 'active_users' not in st.session_state:
+    st.session_state.active_users = 0
+
+# Function to handle login
+def login():
+    if st.session_state.active_users < st.secrets["max_user"]:
+        st.session_state.is_logged_in = True
+        st.session_state.active_users += 1
+        st.success("You are logged in.")
+    else:
+        st.error("Maximum number of users reached. Please try again later.")
+
+# Function to handle logout
+def logout():
+    st.session_state.is_logged_in = False
+    if st.session_state.active_users > 0:
+        st.session_state.active_users -= 1
+    st.success("You are logged out.")
+
+# Function to handle sending email
+def send_email(to_email, subject, message):
+    from_email = st.secrets["email"]["from_email"]
+    from_password = st.secrets["email"]["from_password"]
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_email, from_password)
+        server.send_message(msg)
+        server.quit()
+        st.success("Email sent successfully!")
+    except Exception as e:
+        st.error(f"Failed to send email. Error: {e}")
+
+# Main application logic
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ["Home", "Contact Us", "About Us"])
+
+    if page == "Home":
+        st.title("EduTube Demo")
+        st.write(f"Active users: {st.session_state.active_users} from Maximum {st.secrets['max_user']}")
+
+        # Initialize session state if it doesn't exist
+        if 'is_logged_in' not in st.session_state:
+            st.session_state.is_logged_in = False
+
+        # Display the login button if the user is not logged in
+        if not st.session_state.is_logged_in:
+            st.button("Enter the Demo EduTube Website", on_click=login)
+
+        # Display the main content if the user is logged in
+        if st.session_state.is_logged_in:
+            st.title("Educational Video Recommendation")
+            selected_tag = st.selectbox("Choose an educational tag:", generate_tags())
+            videos = search_videos(selected_tag)
+            st.header(f"Suggested Videos for '{selected_tag}':")
+            for video in videos:
+                video_title = video['snippet']['title']
+                video_id = video['id']['videoId']
+                video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+                st.subheader(video_title)
+                st.video(video_url)
+
+            deep_search()
+
+            if st.button("Logout"):
+                logout()
+                st.experimental_rerun()  # Rerun the script to clear the content
+
+    elif page == "Contact Us":
+        st.title("Contact Us")
+        st.write("If you have any questions or feedback, feel free to contact us using the form below.")
+
+        contact_form = st.form(key='contact_form')
+        with contact_form:
+            email = st.text_input("Your email address")
+            subject = st.text_input("Subject")
+            message = st.text_area("Message")
+            submit_button = st.form_submit_button(label='Send')
+
+        if submit_button:
+            send_email(st.secrets["target_email"], subject, f"From: {email}\n\n{message}")
+
+    elif page == "About Us":
+        st.title("About Us")
+        st.write("""
+            ### EduTube Demo
+            The objective of EduTube Demo is to provide a platform for users to find educational videos based on various advanced and diverse subjects. 
+            By leveraging AI and machine learning, EduTube recommends high-quality educational content to enhance the learning experience.
+        """)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except RequestException:
+        st.error("Error connecting to the APIs. Please try again next 24 hours.")
+
+
